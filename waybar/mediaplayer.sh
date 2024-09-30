1acbf7d86c7a6a4c36418e9275c5eaad6e46f0cd
olxandr <voronko.olexandr@gmail.com>
2024-09-30 18:54:55 +0300

    dfsafsf

---
+++ waybar/mediaplayer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+player_status=$(playerctl --player=spotify status)
+if [ "$player_status" = "Playing" ]; then
+    echo "  $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+elif [ "$player_status" = "Paused" ]; then
+    echo "  $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+else
+    echo 
+fi
